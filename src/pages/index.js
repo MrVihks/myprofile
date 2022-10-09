33cde17e809fb2e79fef75aeb7b69b818c92f87c
MrVihks <viniciushkds@gmail.com>
2022-10-09 19:16:51 -0300

    mudando um pouco a estilização

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Img from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
+
 
 import Styles from '../styles/Home.module.css'
 
@@ -11,7 +13,7 @@ export default function Home() {
         <title>Vinicius Henrique</title>
       </Head>
       <header id={Styles.header}>
-        <div id={Styles.image_div}>
+        <div id={Styles.image_div} >
           <Img src="/images/vini.jpeg"
             width={160}
             height={214}
@@ -21,13 +23,15 @@ export default function Home() {
             id={Styles.image}
           />
         </div>
-        <div id={Styles.title}>
-          <h1>Vinicius Henrique</h1>
-          <h4>Apenas um cara aleatório que ama programar 🤪😎.</h4>
-        </div>
-        <div id={Styles.buttons}>
-          <a href='https://github.com/MrVihks/' target="_blank" rel="noopener noreferrer">Meu GitHub</a>
-          <a href='https://www.instagram.com/mrvihks/' target="_blank" rel="noopener noreferrer">Meu Instagram</a>
+        <div class="head_content">
+          <div id={Styles.title} class="title">
+            <h1>Vinicius Henrique</h1>
+            <h4>Apenas um cara aleatório que ama programar 🤪😎.</h4>
+          </div>
+          <div id={Styles.buttons}>
+            <a href='https://github.com/MrVihks/' target="_blank" rel="noopener noreferrer">Meu GitHub</a>
+            <a href='https://www.instagram.com/mrvihks/' target="_blank" rel="noopener noreferrer">Meu Instagram</a>
+          </div>
         </div>
       </header>
     </>
